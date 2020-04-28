98a1254f6d981e80d62ffa937e9d13d530991426
SanujaLW <sanujalw@gmail.com>
2020-04-28 18:23:31 +0530

    JWKUtils - Added class RS256Key

--- src/JWKUtils.ts
+++ src/JWKUtils.ts
@@ -2,10 +2,168 @@ import { JWK } from 'jose';
 import { eddsa as EdDSA, ec as EC } from 'elliptic';
 import * as base58 from 'bs58';
 import base64url from 'base64url';
+const rsaPemToJWk = require('rsa-pem-to-jwk');
 
 export const ERRORS = Object.freeze({
     INVALID_KEY_FORMAT: 'Invalid key format error',
 });
+
+export namespace KeyObjects{
+    interface BasicKeyObject{
+        kty: string;
+        use: string;
+        kid: string;
+        alg: string;
+    }
+
+    export interface RSAPrivateKeyObject extends BasicKeyObject{
+        p: string;
+        q: string;
+        d: string;
+        e: string;
+        qi: string;
+        dp: string;
+        dq: string;
+        n: string;
+    }
+
+    export interface RSAPublicKeyObject extends BasicKeyObject{
+        e: string;
+        n: string;
+    }
+
+    export interface ECPrivateKeyObject extends BasicKeyObject{
+        crv: string;
+        d: string;
+        x: string;
+        y: string;
+    }
+
+    export interface ECPublicKeyObject extends BasicKeyObject {
+        crv: string;
+        x: string;
+        y: string;
+    }
+
+    export interface OKPPrivateKeyObject extends BasicKeyObject {
+        crv: string;
+        d: string;
+        x: string;
+    }
+
+    export interface OKPPublicKeyObject extends BasicKeyObject {
+        crv: string;
+        x: string;
+    }
+
+    export interface SymmetricKeyObject extends BasicKeyObject {
+        k: string;
+    }
+}
+
+enum KTYS{
+    'RSA',
+    'EC',
+    'OKP',
+    'oct',
+}
+
+enum ALGS{
+    'RS256',
+    'ES256K',
+    'EdDSA',
+}
+
+export class RS256Key{
+    private kty: string;
+    private alg: string;
+    private kid: string;
+    private use: string; 
+    private p?: string;
+    private q?: string;
+    private d?: string;
+    private e: string;
+    private qi?: string;
+    private dp?: string;
+    private dq?: string;
+    private n: string;
+    private isPrivate: boolean;
+
+    constructor(kid: string, kty: KTYS, alg: ALGS, n: string, e: string, sig: boolean){
+        this.kid = kid;
+        this.kty = KTYS[kty];
+        this.alg = ALGS[alg];
+        this.use = sig? 'sig': 'enc';
+        this.n = n;
+        this.e = e;
+        this.isPrivate = false;
+    }
+
+    static fromPublicKey(key: string| KeyObjects.RSAPublicKeyObject, kid: string, sig: boolean = true): RS256Key{
+        if(typeof key === 'object'){
+            return new RS256Key(key.kid, KTYS.RSA, ALGS.RS256, key.n, key.e, sig);
+        }
+        else{
+            let jwk = rsaPemToJWk(key);
+            return new RS256Key(kid, KTYS.RSA, ALGS.RS256, jwk.n, jwk.e, sig);
+        }
+    }
+
+    static fromPrivateKey(key: string | KeyObjects.RSAPrivateKeyObject, kid: string, sig: boolean = false): RS256Key {
+        if (typeof key === 'object') {
+            let rs256Key =  new RS256Key(key.kid, KTYS.RSA, ALGS.RS256, key.n, key.e, sig);
+            rs256Key.isPrivate = true;
+            rs256Key.p = key.p;
+            rs256Key.q = key.q;
+            rs256Key.d = key.d;
+            rs256Key.qi = key.qi;
+            rs256Key.dp = key.dp;
+            rs256Key.dq = key.dq;
+            return rs256Key;
+        }
+        else {
+            let jwk = rsaPemToJWk(key);
+            let rs256Key = new RS256Key(kid, KTYS.RSA, ALGS.RS256, jwk.n, jwk.e, sig);
+            rs256Key.isPrivate = true;
+            rs256Key.p = jwk.p;
+            rs256Key.q = jwk.q;
+            rs256Key.d = jwk.d;
+            rs256Key.qi = jwk.qi;
+            rs256Key.dp = jwk.dp;
+            rs256Key.dq = jwk.dq;
+            return rs256Key;
+        }
+    }
+
+    toJWK(): KeyObjects.RSAPrivateKeyObject | KeyObjects.RSAPublicKeyObject{
+        if(this.isPrivate){
+            return {
+                kty: this.kty,
+                use: this.use,
+                kid: this.kid,
+                alg: this.alg,
+                p: this.p,
+                q: this.q,
+                d: this.d,
+                e: this.e,
+                qi: this.qi,
+                dp: this.dp,
+                dq: this.dq,
+                n: this.n,
+            }
+        }
+        else{
+            return{
+                kty: this.kty,
+                use: this.use,
+                kid: this.kid,
+                alg: this.alg,
+                e: this.e,
+                n: this.n,
+            }
+        }
+    }
+}
 
 export enum KEYFORMATS{
     publicKeyPem,
