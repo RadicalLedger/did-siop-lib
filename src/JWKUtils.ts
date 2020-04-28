7c92d1a68969b0e7f548c01f0d308bd2b4829818
SanujaLW <sanujalw@gmail.com>
2020-04-28 06:04:42 +0530

    JWKUtils - added function getECKey

--- src/JWKUtils.ts
+++ src/JWKUtils.ts
@@ -1,5 +1,5 @@
 import { JWK } from 'jose';
-import { eddsa as EdDSA } from 'elliptic';
+import { eddsa as EdDSA, ec as EC } from 'elliptic';
 import * as base58 from 'bs58';
 import base64url from 'base64url';
 
@@ -50,6 +50,51 @@ export function getOKP(key_str: string, kid: string, keyFormat: KEYFORMATS, isPu
                 "kid": kid,
                 "x": base64url.encode(edKey.getPublic()),
                 "alg": "EdDSA"
+            });
+        }
+    } catch (err) {
+        throw new Error(ERRORS.INVALID_KEY_FORMAT);
+    }
+}
+
+export function getECKey(key_str: string, kid: string, keyFormat: KEYFORMATS, isPublic: boolean = true): JWK.ECKey{
+    let key_buffer = Buffer.alloc(1);
+    try {
+        switch (keyFormat) {
+            case KEYFORMATS.publicKeyBase58: key_buffer = base58.decode(key_str); break;
+            case KEYFORMATS.publicKeyBase64: key_buffer = base64url.toBuffer(base64url.fromBase64(key_str)); break;
+            case KEYFORMATS.publicKeyHex: key_buffer = Buffer.from(key_str, 'hex'); break;
+            default: throw new Error(ERRORS.INVALID_KEY_FORMAT);
+        }
+    } catch (err) {
+        throw new Error(ERRORS.INVALID_KEY_FORMAT);
+    }
+
+    let ec = new EC('secp256k1');
+    let ecKey;
+
+    try {
+        if (isPublic) {
+            ecKey = ec.keyFromPublic(key_buffer);
+            return JWK.asKey({
+                "kty": "EC",
+                "crv": "secp256k1",
+                "kid": kid,
+                "x": base64url.encode(ecKey.getPublic().getX().toArrayLike(Buffer)),
+                "y": base64url.encode(ecKey.getPublic().getY().toArrayLike(Buffer)),
+                "alg": "ES256K"
+            });
+        }
+        else {
+            ecKey = ec.keyFromPrivate(key_buffer);
+            return JWK.asKey({
+                "kty": "EC",
+                "d": base64url.encode(ecKey.getPrivate().toArrayLike(Buffer)),
+                "crv": "secp256k1",
+                "kid": kid,
+                "x": base64url.encode(ecKey.getPublic().getX().toArrayLike(Buffer)),
+                "y": base64url.encode(ecKey.getPublic().getY().toArrayLike(Buffer)),
+                "alg": "ES256K"
             });
         }
     } catch (err) {
--- test/jwk.spec.ts
+++ test/jwk.spec.ts
@@ -1,11 +1,8 @@
 import { JWK } from 'jose';
-//import { eddsa as EdDSA } from 'elliptic';
 import * as JWKUtils from '../src/JWKUtils'
-//import * as base58 from 'bs58';
-//import base64url from 'base64url';
 
-describe('JWK functions',  function(){
-    test('OKP retrieval', async ()=>{
+describe('JWK functions', function () {
+    test('OKP retrieval', async () => {
         let kid = "key_1";
         let expectedPublic = JWK.asKey({
             "kty": "OKP",
@@ -45,6 +42,34 @@ describe('JWK functions',  function(){
 
         let hexStrPrivate = '2e1b082fad782d318ca77cea0f774c9d34d1840e2d51261fa2f0494e7a87d62a';
         received = JWKUtils.getOKP(hexStrPrivate, kid, JWKUtils.KEYFORMATS.publicKeyHex, false);
+        expect(received).toEqual(expectedPrivate);
+    });
+    test('ECKey Retrieval', async () => {
+        let kid = "key_1";
+        let expectedPublic = JWK.asKey({
+            "kty": "EC",
+            "crv": "secp256k1",
+            "kid": "key_1",
+            "x": "dkp-YKIFz9Cg06D07uwLLHj3LbgHjv1HiiiXCJXrE1k",
+            "y": "3QQZB8pJjP7FNoZ19EQjGSorXmxGtj4bs4clyoeB_fY",
+            "alg": "ES256K"
+        });
+        let expectedPrivate = JWK.asKey({
+            "kty": "EC",
+            "d": "9aANYnB315hk6vlQGjKTQj-jKTNjYfmUVoeW0ogLaWg",
+            "crv": "secp256k1",
+            "kid": "key_1",
+            "x": "dkp-YKIFz9Cg06D07uwLLHj3LbgHjv1HiiiXCJXrE1k",
+            "y": "3QQZB8pJjP7FNoZ19EQjGSorXmxGtj4bs4clyoeB_fY",
+            "alg": "ES256K"
+        });
+
+        let hexStrPublic = '02764a7e60a205cfd0a0d3a0f4eeec0b2c78f72db8078efd478a28970895eb1359';
+        let received = JWKUtils.getECKey(hexStrPublic, kid, JWKUtils.KEYFORMATS.publicKeyHex);
+        expect(received).toEqual(expectedPublic);
+
+        let hexStrPrivate = 'f5a00d627077d79864eaf9501a3293423fa329336361f994568796d2880b6968';
+        received = JWKUtils.getECKey(hexStrPrivate, kid, JWKUtils.KEYFORMATS.publicKeyHex, false);
         expect(received).toEqual(expectedPrivate);
     });
 })
