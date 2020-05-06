e29cbfb7a8bdbcd6e630ff0fb6caa111dd0b6a48
SanujaLW <sanujalw@gmail.com>
2020-05-06 18:31:48 +0530

    Response - Added response generation and validation

---
+++ test/response.spec.ts
@@ -0,0 +1,50 @@
+import { DidSiopResponse } from './../src/Response';
+import { Identity } from './../src/Identity';
+import { SigningInfo } from './../src/JWT';
+import { ALGORITHMS, KTYS, KEY_FORMATS } from '../src/globals';
+import { ECKey } from '../src/JWKUtils';
+
+describe("Response", function () {
+    test("Response generation and validation", async () => {
+        jest.setTimeout(7000);
+        let requestPayload = {
+            "iss": "did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83",
+            "response_type": "id_token",
+            "client_id": "https://my.rp.com/cb",
+            "scope": "openid did_authn",
+            "state": "af0ifjsldkj",
+            "nonce": "n-0S6_WzA2Mj",
+            "response_mode": "form_post",
+            "registration": {
+                "jwks_uri": "https://uniresolver.io/1.0/identifiers/did:example:0xab;transform-keys=jwks",
+                "id_token_signed_response_alg": ["ES256K", "ES256K-R", "EdDSA", "RS256"]
+            }
+        }
+        let signing: SigningInfo = {
+            alg: ALGORITHMS["ES256K-R"],
+            privateKey: ECKey.fromKey({
+                key: 'CE438802C1F0B6F12BC6E686F372D7D495BC5AA634134B4A7EA4603CB25F0964',
+                kid: 'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner',
+                use: 'sig',
+                kty: KTYS[KTYS.EC],
+                format: KEY_FORMATS.HEX,
+                isPrivate: true,
+            }),
+            publicKey_kid: 'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner',
+        }
+
+        let user = new Identity();
+        await user.resolve('did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83')
+
+        let response = await DidSiopResponse.generateResponse(requestPayload, signing, user, 5000);
+
+        let checkParams = {
+            redirect_uri: 'https://my.rp.com/cb',
+            nonce: "n-0S6_WzA2Mj",
+            validBefore: 1000,
+            isExpirable: true,
+        }
+        let validity = await DidSiopResponse.validateResponse(response, checkParams);
+        expect(validity).toBeTruthy();
+    });
+});
