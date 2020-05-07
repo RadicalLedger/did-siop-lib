080e0b4a757bfe7e6a349de87419c097a4bf1c65
SanujaLW <sanujalw@gmail.com>
2020-05-07 13:13:11 +0530

    Added index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { SIOP as Provider } from './DID_SIOP';
+export { RP } from './DID_SIOP_RP';
+export { DidDocument } from './Identity';
+export { SigningInfo } from './JWT';
+export { JWTObject } from './JWT';
+export { CheckParams }  from './Response';
