682ede7da67ef1cff2faf0d6cd0620592c8ac450
SanujaLW <sanujalw@gmail.com>
2020-05-12 11:48:47 +0530

    Index - Removed SigningInfo and added KEY_FORMATS

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { SIOP as Provider } from './DID_SIOP';
 export { RP } from './DID_SIOP_RP';
 export { DidDocument } from './Identity';
-export { SigningInfo } from './JWT';
 export { ALGORITHMS } from './globals';
+export { KEY_FORMATS } from './globals';
 export { JWTObject } from './JWT';
 export { CheckParams }  from './Response';
