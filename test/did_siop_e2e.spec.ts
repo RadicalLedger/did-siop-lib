65507d360ad6409c71f2357794ed60f071aa63f8
SanujaLW <sanujalw@gmail.com>
2020-05-19 02:46:40 +0530

    SIOP e2e - Extended tests

--- test/did_siop_e2e.spec.ts
+++ test/did_siop_e2e.spec.ts
@@ -1,81 +1,107 @@
+import { ERROR_RESPONSES } from './../src/core/ErrorResponse';
 import { JWTObject } from './../src/core/JWT';
 import { Provider } from './../src/core/Provider';
 import { RP } from '../src/core/RP';
 import { ALGORITHMS, KEY_FORMATS } from '../src/core/globals';
 
-describe('DID SIOP', function () {
-    test('DID SIOP end to end functions testing', async () => {
-        jest.setTimeout(10000);
-        
-        let requestObj: JWTObject = {
-            header: {
-                "alg": "ES256K-R",
-                "typ": "JWT",
-                "kid": "did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner"
-            },
-            payload:{
-                "iss": "did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83",
-                "response_type": "id_token",
-                "scope": "openid did_authn",
-                "client_id": "https://my.rp.com/cb",
-                "registration": {
-                  "jwks_uri": "https://uniresolver.io/1.0/identifiers/did:example:0xab;transform-keys=jwks",
-                  "id_token_signed_response_alg": [
-                    "ES256K",
-                    "ES256K-R",
-                    "EdDSA",
-                    "RS256"
-                  ]
-                }
-              }
+let requestObj: JWTObject = {
+    header: {
+        "alg": "ES256K-R",
+        "typ": "JWT",
+        "kid": "did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner"
+    },
+    payload:{
+        "iss": "did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83",
+        "response_type": "id_token",
+        "scope": "openid did_authn",
+        "client_id": "https://my.rp.com/cb",
+        "registration": {
+          "jwks_uri": "https://uniresolver.io/1.0/identifiers/did:example:0xab;transform-keys=jwks",
+          "id_token_signed_response_alg": [
+            "ES256K",
+            "ES256K-R",
+            "EdDSA",
+            "RS256"
+          ]
         }
+      }
+}
 
-        let rp = await RP.getRP(
-            'https://my.rp.com/cb',
-            'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83',
-            {
-                "jwks_uri": "https://uniresolver.io/1.0/identifiers/did:example:0xab;transform-keys=jwks",
-                "id_token_signed_response_alg": ["ES256K", "ES256K-R", "EdDSA", "RS256"]
-            }
-        );
+let badRequest = 'openid://?response_type=id_token&client_id=https://rp.example.com/cb&scope=openid did_authn&request=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiIsImtpZCI6ImRpZDpldGhyOjB4QjA3RWFkOTcxN2I0NEI2Y0Y0MzljNDc0MzYyYjlCMDg3N0NCQkY4MyNvd25lciJ9.eyJyZXNwb25zZV90eXBlIjoiaWRfdG9rZW4iLCJjbGllbnRfaWQiOiJodHRwczovL215LnJwLmNvbS9jYiIsInNjb3BlIjoib3BlbmlkIGRpZF9hdXRobiIsInN0YXRlIjoiYWYwaWZqc2xka2oiLCJub25jZSI6Im4tMFM2X1d6QTJNaiIsInJlc3BvbnNlX21vZGUiOiJmb3JtX3Bvc3QiLCJyZWdpc3RyYXRpb24iOnsiandrc191cmkiOiJodHRwczovL3VuaXJlc29sdmVyLmlvLzEuMC9pZGVudGlmaWVycy9kaWQ6ZXhhbXBsZToweGFiO3RyYW5zZm9ybS1rZXlzPWp3a3MiLCJpZF90b2tlbl9zaWduZWRfcmVzcG9uc2VfYWxnIjpbIkVTMjU2SyIsIkVkRFNBIiwiUlMyNTYiXX19.mXh9VLcxzHFt3D1EFRQm0xDfPB7P4YbnZX2u8Lm46mU4TIbBDqx49tyVMeAx2BCRORAN__JXS2U4NpVheAaX2wA';
 
-        let rpPrivateKey = 'CE438802C1F0B6F12BC6E686F372D7D495BC5AA634134B4A7EA4603CB25F0964';
-        let rpKid = 'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner';
+let rpDID = 'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83';
+let rpRedirectURI = 'https://my.rp.com/cb';
+let rpRegistrationMetaData = {
+        "jwks_uri": "https://uniresolver.io/1.0/identifiers/did:example:0xab;transform-keys=jwks",
+        "id_token_signed_response_alg": ["ES256K", "ES256K-R", "EdDSA", "RS256"]
+};
+let rpPrivateKey = 'CE438802C1F0B6F12BC6E686F372D7D495BC5AA634134B4A7EA4603CB25F0964';
+let rpKid = 'did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner';
 
+let userDID = 'did:ethr:0x30D1707AA439F215756d67300c95bB38B5646aEf';
+let userPrivateKeyHex = '3f81cb66c8cbba18fbe25f99d2fb4e19f54a1ee69c335ce756a705726189c9e7';
+let userKid = 'did:ethr:0x30D1707AA439F215756d67300c95bB38B5646aEf#owner';
+
+describe('DID SIOP', function () {
+    test('DID SIOP end to end functions testing - expect truthy', async () => {
+        jest.setTimeout(10000);
+
+        let rp = await RP.getRP(rpRedirectURI, rpDID, rpRegistrationMetaData);
         rp.addSigningParams(rpPrivateKey, rpKid, KEY_FORMATS.HEX, ALGORITHMS["ES256K-R"]);
 
+        let provider = new Provider();
+        await provider.setUser(userDID);
+        provider.addSigningParams(userPrivateKeyHex, userKid, KEY_FORMATS.HEX, ALGORITHMS["ES256K-R"]);
+
         let request =  await rp.generateRequest();
-
-        let siop = new Provider();
-        await siop.setUser('did:ethr:0x30D1707AA439F215756d67300c95bB38B5646aEf');
-
-        let userPrivateKeyHex = '3f81cb66c8cbba18fbe25f99d2fb4e19f54a1ee69c335ce756a705726189c9e7';
-        let userKid = 'did:ethr:0x30D1707AA439F215756d67300c95bB38B5646aEf#owner';
-
-        siop.addSigningParams(userPrivateKeyHex, userKid, KEY_FORMATS.HEX, ALGORITHMS["ES256K-R"]);
-
-        let requestJWTDecoded = await siop.validateRequest(request);
-
+        let requestJWTDecoded = await provider.validateRequest(request);
         expect(requestJWTDecoded).toMatchObject(requestObj);
 
-        let response = await siop.generateResponse(requestJWTDecoded.payload);
-
+        let response = await provider.generateResponse(requestJWTDecoded.payload);
         let responseJWTDecoded = await rp.validateResponse(response, {
-            redirect_uri: 'https://my.rp.com/cb',
+            redirect_uri: rpRedirectURI,
             isExpirable: true,
         })
-
         expect(responseJWTDecoded).toHaveProperty('header');
         expect(responseJWTDecoded).toHaveProperty('payload');
+    });
+    test('DID SIOP end to end functions testing - expect falsy', async () => {
+        jest.setTimeout(10000);
+
+        let rp = await RP.getRP(rpRedirectURI, rpDID, rpRegistrationMetaData);
+        rp.addSigningParams(rpPrivateKey, rpKid, KEY_FORMATS.HEX, ALGORITHMS["ES256K-R"]);
+
+        let provider = new Provider();
+        await provider.setUser(userDID);
+        provider.addSigningParams(userPrivateKeyHex, userKid, KEY_FORMATS.HEX, ALGORITHMS["ES256K-R"]);
 
         rp.removeSigningParams('did:ethr:0xB07Ead9717b44B6cF439c474362b9B0877CBBF83#owner');
         let requestPromise = rp.generateRequest();
-
         expect(requestPromise).rejects.toEqual(new Error('Atleast one SigningInfo is required'));
 
-        siop.removeSigningParams('did:ethr:0x30D1707AA439F215756d67300c95bB38B5646aEf#owner');
-        let responsePromise = siop.generateResponse(requestJWTDecoded.payload);
-
+        provider.removeSigningParams('did:ethr:0x30D1707AA439F215756d67300c95bB38B5646aEf#owner');
+        let responsePromise = provider.generateResponse(requestObj.payload);
         expect(responsePromise).rejects.toEqual(new Error('Atleast one SigningInfo is required'));
+    });
+    test('DID SIOP end to end functions testing - Error Response', async () => {
+        jest.setTimeout(10000);
+
+        let rp = await RP.getRP(rpRedirectURI, rpDID, rpRegistrationMetaData);
+        rp.addSigningParams(rpPrivateKey, rpKid, KEY_FORMATS.HEX, ALGORITHMS["ES256K-R"]);
+
+        let provider = new Provider();
+        await provider.setUser(userDID);
+        provider.addSigningParams(userPrivateKeyHex, userKid, KEY_FORMATS.HEX, ALGORITHMS["ES256K-R"]);
+
+        let requestValidationError = new Error('Unknown error');
+        try{
+            await provider.validateRequest(badRequest);
+        }
+        catch(err){
+            requestValidationError = err;
+        }
+        let errorResponse = provider.generateErrorResponse(requestValidationError.message);
+        let errorResponseDecoded = await rp.validateResponse(errorResponse);
+        expect(errorResponseDecoded).toEqual(ERROR_RESPONSES.invalid_request_object.response);
     })
 })
