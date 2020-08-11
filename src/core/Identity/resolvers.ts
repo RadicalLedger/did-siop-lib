10add3980147ed2bf45535f3c116871d72a88473
SanujaLW <59592997+SanujaLW@users.noreply.github.com>
2020-08-12 01:42:54 +0530

    Identity - Added did:key resolver

--- src/core/Identity/resolvers.ts
+++ src/core/Identity/resolvers.ts
@@ -1,6 +1,10 @@
 import { DidDocument } from "./commons";
 import { ERRORS } from "./commons";
 import { getResolver } from 'ethr-did-resolver';
+import * as base58 from 'bs58';
+import multibase from "multibase";
+import multicodec from 'multicodec';
+import ed2curve from 'ed2curve';
 const axios = require('axios').default;
 
 abstract class DidResolver{
@@ -49,6 +53,56 @@ class EthrDidResolver extends DidResolver{
     }
 }
 
+class KeyDidResolver extends DidResolver{
+    resolve(did: string): Promise<DidDocument> {
+        if(!did) {
+            throw new TypeError('"did" must be a string.');
+        }
+        
+        const didAuthority = did.split('#')[0];
+        const fingerprint = didAuthority.substr('did:key:'.length);
+        
+        const decodedFingerprint = multibase.decode(fingerprint);
+        const unprefixed = multicodec.rmPrefix(decodedFingerprint);
+        const publicKey = base58.encode(unprefixed);
+        const keyId = did + '#' + fingerprint;
+
+        const keyAgreementKeyBuffer = ed2curve.convertPublicKey(unprefixed);
+        if(!keyAgreementKeyBuffer) throw new Error('Cannot derive keyAgreement');
+        const keyAgreementKey = base58.encode(keyAgreementKeyBuffer);
+        
+        const keyAgreementIdBuffer = Buffer.alloc(2 + keyAgreementKeyBuffer.length);
+        keyAgreementIdBuffer[0] = 0xec;
+        keyAgreementIdBuffer[1] = 0x01;
+        keyAgreementIdBuffer.set(keyAgreementKeyBuffer, 2);
+        const keyAgreementId = did + '#' + 'z' + base58.encode(keyAgreementIdBuffer);
+
+        const didDoc = {
+            '@context': ['https://w3id.org/did/v0.11'],
+            id: did,
+            publicKey: [{
+                id: keyId,
+                type: 'Ed25519VerificationKey2018',
+                controller: did,
+                publicKeyBase58: publicKey
+            }],
+            authentication: [keyId],
+            assertionMethod: [keyId],
+            capabilityDelegation: [keyId],
+            capabilityInvocation: [keyId],
+            keyAgreement: [{
+                id: keyAgreementId,
+                type: 'X25519KeyAgreementKey2019',
+                controller: did,
+                publicKeyBase58: keyAgreementKey
+            }]
+        };
+        
+        console.log('resolved by did:key\n' + JSON.stringify(didDoc));
+        return Promise.resolve(didDoc);
+    }
+}
+
 class UniversalDidResolver extends DidResolver{
     async resolve(did: string): Promise<DidDocument>{
         let returned = await axios.get('https://dev.uniresolver.io/1.0/identifiers/' + did);
@@ -60,4 +114,5 @@ class UniversalDidResolver extends DidResolver{
 
 export const combinedDidResolver = new CombinedDidResolver()
     .addResolver(new EthrDidResolver())
+    .addResolver(new KeyDidResolver())
     .addResolver(new UniversalDidResolver());
