e50e179ba5fcc7d33c23e7b8173f407824e331e2
SanujaLW <59592997+SanujaLW@users.noreply.github.com>
2020-09-10 17:57:24 +0530

    Added DHSecretGenerator class

--- src/core/JWE.ts
+++ src/core/JWE.ts
@@ -1,4 +1,4 @@
-import { Key } from "./JWKUtils";
+import { Key, ECKey, OKP } from "./JWKUtils";
 import base64url from 'base64url';
 import { KTYS, KEY_FORMATS, ALGORITHMS } from "./globals";
 import { ec as EC } from 'elliptic';
@@ -28,6 +28,7 @@ export interface JWTEncryptedObject{
 
 export const ERRORS = Object.freeze({
     INVALID_JWE: 'Invalid JWE',
+    UNSUPPORTED_CURVE: 'Unsupported curve',
 });
 
 function decodeJWT(jwe: string): JWTEncryptedObject{
@@ -46,4 +47,43 @@ function decodeJWT(jwe: string): JWTEncryptedObject{
     } catch (err) {
         throw new Error(ERRORS.INVALID_JWE);
     }
+}
+
+abstract class DHSecretGenerator{
+    abstract generate(publicKey: Key, ephemeralKeyPair?: Key): any;
+    abstract regenerate(privateKey: Key, ephemeralPublicKey: Key): Buffer;
+}
+
+class ECDHSecretGenerator extends DHSecretGenerator{
+    
+    private curves = ['secp256k1', 'p192', 'p224', 'p256', 'p384', 'p521', 'curve25519'];
+
+    generate(publicKey: ECKey | OKP, ephemeralPrivateKey?: ECKey | OKP): any {
+        let curve;
+        if(this.curves.includes(publicKey.getMinimalJWK().crv)){
+            curve = publicKey.getMinimalJWK().crv;
+        }
+        else{
+            throw new Error(ERRORS.UNSUPPORTED_CURVE);
+        }
+
+        if(!ephemeralPrivateKey){
+            const ec = new EC(curve);
+            let keyPair = ec.genKeyPair();
+
+            ephemeralPrivateKey = ECKey.fromKey({
+                key: keyPair.getPrivate().toString(16),
+                kid: '',
+                use: 'enc',
+                kty: 'EC',
+                format: KEY_FORMATS.HEX,
+                isPrivate: true,
+            });
+        }
+    }
+
+    regenerate(privateKey: ECKey | OKP, ephemeralPublicKey: ECKey | OKP): Buffer {
+        throw new Error("Method not implemented.");
+    }
+
 }
