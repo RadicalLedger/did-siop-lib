5fa624b654605b5678dba2fa999d6ebee2071dec
Chim Himidumage <chim@cmbp.local>
2022-04-21 07:54:28 +0530

    basic tests for response generation with claims

--- test/common.spec.ts
+++ test/common.spec.ts
@@ -1,7 +1,70 @@
-import { JWTObject, SigningInfo } from '../src/core/JWT';
-import { sign } from '../src/core/JWT';
+import { JWTObject } from '../src/core/JWT';
 
-export const getModifiedJWT = function (jwt: JWTObject, privateKey: SigningInfo, isPayload: boolean, property: string, value?: any) {    
+const jwtGoodDecoded = {
+    header: {
+        "typ": "JWT",
+        "alg": "ES256K",
+        "kid": "",
+    },
+    payload: {
+        "iss": "",
+        "response_type": "id_token",
+        "client_id": "https://my.rp.com/cb",
+        "scope": "openid did_authn",
+        "state": "af0ifjsldkj",
+        "nonce": "n-0S6_WzA2Mj",
+        "response_mode": "form_post",
+        "registration": {
+            "jwks_uri": "https://uniresolver.io/1.0/identifiers/did:example:0xab;transform-keys=jwks",
+            "id_token_signed_response_alg": ["ES256K", "EdDSA", "RS256"]
+        }
+    }
+}
+
+export const claims ={
+    good:{
+        "id_token": {
+            "email": null
+        },
+        "vp_token": {
+            "presentation_definition": {
+                "id": "vp token example",
+                "input_descriptors": [
+                    {
+                        "id": "id card credential",
+                        "format": {
+                            "ldp_vc": {
+                                "proof_type": [
+                                    "Ed25519Signature2018"
+                                ]
+                            }
+                        },
+                        "constraints": {
+                            "fields": [
+                                {
+                                    "path": [
+                                        "$.type"
+                                    ],
+                                    "filter": {
+                                        "type": "string",
+                                        "pattern": "IDCardCredential"
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    bad:{
+        "id_token": {
+            "email": null
+        },
+        "vp_token": {}    
+    }
+}
+export const getModifiedJWT = function (jwt: JWTObject, isPayload: boolean, property: string, value?: any) {    
     let newJWT = JSON.parse(JSON.stringify(jwt));
     if (isPayload) {
         if (value === null) {
@@ -18,5 +81,14 @@ export const getModifiedJWT = function (jwt: JWTObject, privateKey: SigningInfo,
             newJWT.header[property] = value;
         }
     }
-    return sign(newJWT, privateKey);
+    return newJWT;
+}
+
+export const getBasicJWT = function(kid:string, iss:string):JWTObject{
+
+    let clonedJWT = JSON.parse(JSON.stringify(jwtGoodDecoded)) //To make a deep copy in an ugly way
+    clonedJWT.header["kid"] = kid;
+    clonedJWT.header["iss"] = iss;
+
+    return clonedJWT;
 }
