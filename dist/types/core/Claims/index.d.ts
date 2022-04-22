d793a87b4dc1c98f0d0fb154652e2c2b737c7df4
Chim Himidumage <chim@cmbp.local>
2022-04-22 08:50:33 +0530

    dist update for Claims

---
+++ dist/types/core/Claims/index.d.ts
@@ -0,0 +1,7 @@
+/** @param {any} decodedPayload - Decoded payload of the JWT
+ * @returns {boolean>} - true if all optional parameters are valid, false otherwise
+ * @remarks This method is used to validate optional elements of the Authentication Request.
+ * Currently valuidates claims/vp_token property if exist
+ * https://openid.net/specs/openid-connect-4-verifiable-presentations-1_0.html#name-vp_token
+ */
+export declare function validateRequestJWTClaims(decodedPayload: any): Promise<any>;
