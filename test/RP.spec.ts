bcea03cbd890717de3c78cacbad08e5351461d74
Chim Himidumage <chim@cmbp.local>
2022-04-28 20:53:43 +0530

    Basic unit tests added to RP

---
+++ test/RP.spec.ts
@@ -0,0 +1,31 @@
+import { RP} from '../src/core/RP';
+import {  ERRORS as ID_ERRORS } from '../src/core/Identity';
+// import nock from 'nock';
+import {DID_TEST_RESOLVER_DATA_NEW } from './did_doc.spec.resources'
+
+let registration = {
+        "jwks_uri": "https://uniresolver.io/1.0/identifiers/did:example:0xab;transform-keys=jwks",
+        "id_token_signed_response_alg": ["ES256K-R", "EdDSA", "RS256"]
+        };
+let redirect_uri : 'localhost:4200/home';
+
+describe("RP related function", function() {
+    jest.setTimeout(30000);
+    test("getRP shoud return a valid RP instance with ", async () => {
+        let siop_rp = await RP.getRP(
+            redirect_uri, // RP's redirect_uri
+            DID_TEST_RESOLVER_DATA_NEW[0].did, // RP's did
+            registration
+        )
+        expect(siop_rp).not.toBe(null);            
+    });
+    test("getRP shoud return an if the DID is invalid ", async () => {
+        let siop_rp = RP.getRP(
+            redirect_uri, // RP's redirect_uri
+            "not_a_did", // RP's did
+            registration
+        )
+        await expect(siop_rp).rejects.toEqual(new Error(ID_ERRORS.DOCUMENT_RESOLUTION_ERROR));
+        expect(siop_rp).not.toBe(null);            
+    });
+});
