f93f7ec1c7a85936701359928404c1ba01d8600e
Chim Himidumage <chim@cmbp.local>
2022-04-28 05:52:34 +0530

    vp_token validations test cases

--- test/common.spec.resources.ts
+++ test/common.spec.resources.ts
@@ -56,13 +56,118 @@ export const claims ={
                     }
                 ]
             }
-        }
+        },
     },
     bad:{
         "id_token": {
             "email": null
         },
-        "vp_token": {}    
+        "vp_token": {}
+    }
+}
+export const tokenData ={
+    good:{
+        singleVP : {
+            id_token :{
+                "iss": "https://self-issued.me/v2",
+                "aud": "https://book.itsourweb.org:3000/client_api/authresp/uhn",
+                "iat": 1615910538,
+                "exp": 1615911138,
+                "sub": "NzbLsXh8uDCcd-6MNwXF4W_7noWXFZAfHkxZsRGC9Xs",
+                "sub_jwk": {
+                    "kty": "RSA",
+                    "n": "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx...DKgw",
+                    "e": "AQAB"
+                },
+                "auth_time": 1615910535,
+                "nonce": "960848874",
+                "_vp_token": {
+                    "presentation_submission": {
+                        "id": "Selective disclosure example presentation",
+                        "definition_id": "Selective disclosure example",
+                        "descriptor_map": [
+                            {
+                                "id": "ID Card with constraints",
+                                "format": "ldp_vp",
+                                "path": "$",
+                                "path_nested": {
+                                    "format": "ldp_vc",
+                                    "path": "$.verifiableCredential[0]"
+                                }
+                            }
+                        ]
+                    }
+                }
+            },
+            vp_token : {
+                "@context": [
+                    "https://www.w3.org/2018/credentials/v1"
+                ],
+                "type": [
+                    "VerifiablePresentation"
+                ],
+                "verifiableCredential": [
+                    {
+                        "@context": [
+                            "https://www.w3.org/2018/credentials/v1",
+                            "https://www.w3.org/2018/credentials/examples/v1"
+                        ],
+                        "id": "https://example.com/credentials/1872",
+                        "type": [
+                            "VerifiableCredential",
+                            "IDCardCredential"
+                        ],
+                        "issuer": {
+                            "id": "did:example:issuer"
+                        },
+                        "issuanceDate": "2010-01-01T19:23:24Z",
+                        "credentialSubject": {
+                            "given_name": "Fredrik",
+                            "family_name": "Str&#246;mberg",
+                            "birthdate": "1949-01-22"
+                        },
+                        "proof": {
+                            "type": "Ed25519Signature2018",
+                            "created": "2021-03-19T15:30:15Z",
+                            "jws": "eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..PT8yCqVjj5ZHD0W36zsBQ47oc3El07WGPWaLUuBTOT48IgKI5HDoiFUt9idChT_Zh5s8cF_2cSRWELuD8JQdBw",
+                            "proofPurpose": "assertionMethod",
+                            "verificationMethod": "did:example:issuer#keys-1"
+                        }
+                    }
+                ],
+                "id": "ebc6f1c2",
+                "holder": "did:example:holder",
+                "proof": {
+                    "type": "Ed25519Signature2018",
+                    "created": "2021-03-19T15:30:15Z",
+                    "challenge": "n-0S6_WzA2Mj",
+                    "domain": "https://client.example.org/cb",
+                    "jws": "eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..GF5Z6TamgNE8QjE3RbiDOj3n_t25_1K7NVWMUASe_OEzQV63GaKdu235MCS3hIYvepcNdQ_ZOKpGNCf0vIAoDA",
+                    "proofPurpose": "authentication",
+                    "verificationMethod": "did:example:holder#key-1"
+                }
+            }
+        },
+
+    },
+    bad:{
+        singleVP : {
+            id_token : {
+                _vp_token:{}
+            },
+            vp_token : {
+                test:{}                
+            }
+        }
+    }
+}
+
+export const vpdata ={
+    good:{
+        singleVP : {}
+    },
+    bad:{
+        singleVP : {"vp_token": {}}
     }
 }
 export const getModifiedJWT = function (jwt: JWTObject, isPayload: boolean, property: string, value?: any) {    
--- test/response.spec.ts
+++ test/response.spec.ts
@@ -1,11 +1,13 @@
 import { ERROR_RESPONSES } from '../src/core/ErrorResponse';
 import { DidSiopResponse } from '../src/core/Response';
 import { Identity } from '../src/core/Identity';
+import { SIOPTokensEcoded, VPData } from '../src/core/Claims';
 import { SigningInfo } from '../src/core/JWT';
 import { ALGORITHMS, KEY_FORMATS } from '../src/core/globals';
 import nock from 'nock';
 import { requestJWT } from './response.spec.resources';
 import {DID_TEST_RESOLVER_DATA_NEW as DIDS } from './did_doc.spec.resources'
+import { tokenData } from './common.spec.resources';
 
 let userDidDoc  = DIDS[0].resolverReturn.didDocument;
 let userDID     = DIDS[0].did;
@@ -28,7 +30,7 @@ let checkParams = {
     isExpirable: true,
 }
 
-describe("Response", function () {
+describe("Response with the id_token", function () {
     beforeEach(() => {
         nock('https://uniresolver.io/1.0/identifiers').persist().get('/'+rpDID).reply(200, rpDidDoc).get('/'+userDID).reply(200, userDidDoc);
     });
@@ -41,7 +43,13 @@ describe("Response", function () {
         let validity = await DidSiopResponse.validateResponse(response, checkParams);
         expect(validity).toBeTruthy();
     });
-    test(" with valid vp_token : generation and validation", async () => {
+});
+
+describe("Response validation for a request with a vp_token", function () {
+    beforeEach(() => {
+        nock('https://uniresolver.io/1.0/identifiers').persist().get('/'+rpDID).reply(200, rpDidDoc).get('/'+userDID).reply(200, userDidDoc);
+    });
+    test(" Valid token : generation and validation", async () => {
         jest.setTimeout(30000);
         let user = new Identity();
         await user.resolve(userDID)
@@ -50,12 +58,45 @@ describe("Response", function () {
         let validity = await DidSiopResponse.validateResponse(response, checkParams);
         expect(validity).toBeTruthy();
     });
-    test(" with invalid vp_token : generation", async () => {
+    test(" Invalid token : generation", async () => {
         jest.setTimeout(30000);
         let user = new Identity();
         await user.resolve(userDID)
 
         let validityPromise = DidSiopResponse.generateResponse(requestJWT.bad.withVPToken.payload, signing, user, 30000);
-        await expect(validityPromise).rejects.toEqual(ERROR_RESPONSES.vp_token_missing_presentation_definition.err);
+        await expect(validityPromise).rejects.toEqual(ERROR_RESPONSES.vp_token_missing_presentation_definition.response.error);
+    });
+});
+
+describe("Response generation and validation with vp_token data", function () {
+    beforeEach(() => {
+        nock('https://uniresolver.io/1.0/identifiers').persist().get('/'+rpDID).reply(200, rpDidDoc).get('/'+userDID).reply(200, userDidDoc);
+    });
+    test(" Valid vp_token & _vp_token", async () => {
+        jest.setTimeout(30000);
+        let user = new Identity();
+        await user.resolve(userDID)
+
+        let vps: VPData = {
+            vp_token : tokenData.good.singleVP.vp_token,
+            _vp_token : tokenData.good.singleVP.id_token._vp_token
+        };
+
+        let response:SIOPTokensEcoded = await DidSiopResponse.generateResponseWithVPData(requestJWT.good.withVPToken.payload, signing, user, 30000, vps);
+        let validity = await DidSiopResponse.validateResponse(response.id_token, checkParams);
+        expect(validity).toBeTruthy();
+    });
+    test(" Invalid vp_token : generation should raise an exception", async () => {
+        jest.setTimeout(30000);
+        let user = new Identity();
+        await user.resolve(userDID)
+
+        let bad_vp: VPData = {
+            vp_token : tokenData.bad.singleVP.vp_token,
+            _vp_token : tokenData.bad.singleVP.id_token._vp_token
+        };
+
+        let response =  DidSiopResponse.generateResponseWithVPData(requestJWT.good.withVPToken.payload, signing, user, 30000, bad_vp);
+        await expect(response).rejects.toEqual(ERROR_RESPONSES.vp_token_missing_verifiableCredential.err);
     });
 });
