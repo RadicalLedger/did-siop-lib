f1540d9abff8d0c18d4d44da886d0c9c96c15578
Chim Himidumage <chim@cmbp.local>
2022-05-05 10:59:27 +0530

    comments added

--- src/core/Claims/commons.ts
+++ src/core/Claims/commons.ts
@@ -1,10 +1,10 @@
 export interface VPData{
-    vp_token: any;
-    _vp_token: any;
+    vp_token: any;  // JSON object with VP related data
+    _vp_token: any; // JSON object wit VP request related info
 }
 
 
 export interface SIOPTokensEcoded {
-    id_token: string;
-    vp_token: string;
+    id_token: string; // Base64 encoded JWT
+    vp_token: string; // Base64 encoded JWT
 }
