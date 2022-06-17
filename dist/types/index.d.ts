f3a8f6779795a7d599e227983c12d77f9e9a4008
Chim Himidumage <chim@fidenz.com>
2022-06-17 16:52:28 +0530

    Delete dist directory

--- dist/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { Provider } from './core/Provider';
-export { RP } from './core/RP';
-export { DidDocument, Resolvers } from './core/Identity';
-export { ALGORITHMS, KEY_FORMATS, KTYS, SIOP_METADATA_SUPPORTED } from './core/globals';
-export { JWTObject } from './core/JWT';
-export { CheckParams } from './core/Response';
-export { SIOPError, SIOPErrorResponse, ERROR_RESPONSES } from './core/ErrorResponse';
-export { VPData, SIOPTokensEcoded, SIOPTokenObjects } from './core/Claims';
