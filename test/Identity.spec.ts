d988728e199f5ad03d5bdcda21ac65dbccaa9414
Chim Himidumage <chim@cmbp.local>
2022-06-25 11:50:24 +0530

    Test with Dynamic resolver and refactoring

--- test/Identity.spec.ts
+++ test/Identity.spec.ts
@@ -1,18 +1,30 @@
 import { Identity} from '../src/core/Identity';
-// import nock from 'nock';
+import { KeyDidResolver } from '../src/core/Identity/Resolvers';
 import {DID_TEST_RESOLVER_DATA_NEW } from './did_doc.spec.resources'
 
-describe("NEW Identity2 functions", function() {
+describe("002. Identity functions", function() {
     jest.setTimeout(17000);
-    test("0.NEW Tests resolve(did)", async () => {
-        let identity;
+    test("a. Using default resolver", async () => {
         let resolverData = DID_TEST_RESOLVER_DATA_NEW[0]
-        // nock('https://dev.uniresolver.io/1.0/identifiers/').get('/' + resolverData.did).reply(200, resolverData.resolverReturn).get('/' + invalidDID).reply(404, 'Not found');
 
-        identity = new Identity();
+        let identity = new Identity();
+
         let resolvedDID = await identity.resolve(resolverData.did);
         expect(resolvedDID).toEqual(resolverData.did);
         expect(identity.isResolved()).toBeTruthy();
     
     });
+    test("b. Using soecific resolver (did:key)", async () => {
+        let resolverData = DID_TEST_RESOLVER_DATA_NEW[3] // did:key
+        let keyResolver = new KeyDidResolver('key');
+
+        let identity = new Identity();
+        identity.addResolvers([keyResolver])
+
+        let resolvedDID = await identity.resolve(resolverData.did);
+        expect(resolvedDID).toEqual(resolverData.did);
+        expect(identity.isResolved()).toBeTruthy();
+    
+    });
+
 });
