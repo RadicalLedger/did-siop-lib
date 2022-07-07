9dc9ed8681a77fd437df6a083f1ba0c206d74719
Chim Himidumage <chim@cmbp.local>
2022-07-07 18:04:20 +0530

    Use new format of data

--- test/identity.spec.ts
+++ test/identity.spec.ts
@@ -1,27 +1,26 @@
 import { Identity } from "../src/core/identity";
 import { KeyDidResolver } from "../src/core/identity/resolvers";
-import { DID_TEST_RESOLVER_DATA_NEW } from "./did-doc.spec.resources";
+import { TD_DID_DOCS } from "./data/did-docs.testdata";
 
 describe("002. Identity functions", function () {
   jest.setTimeout(17000);
   test("a. Using default resolver", async () => {
-    let resolverData = DID_TEST_RESOLVER_DATA_NEW[0];
-
     let identity = new Identity();
+    let did1 = TD_DID_DOCS.ethr_rinkeby_1.didDocument.id;
 
-    let resolvedDID = await identity.resolve(resolverData.did);
-    expect(resolvedDID).toEqual(resolverData.did);
+    let resolvedDID = await identity.resolve(did1);
+    expect(resolvedDID).toEqual(did1);
     expect(identity.isResolved()).toBeTruthy();
   });
   test("b. Using soecific resolver (did:key)", async () => {
-    let resolverData = DID_TEST_RESOLVER_DATA_NEW[3]; // did:key
     let keyResolver = new KeyDidResolver("key");
+    let did2 = TD_DID_DOCS.key_2018_2.didDocument.id;
 
     let identity = new Identity();
     identity.addResolvers([keyResolver]);
 
-    let resolvedDID = await identity.resolve(resolverData.did);
-    expect(resolvedDID).toEqual(resolverData.did);
+    let resolvedDID = await identity.resolve(did2);
+    expect(resolvedDID).toEqual(did2);
     expect(identity.isResolved()).toBeTruthy();
   });
 });
