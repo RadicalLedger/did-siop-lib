d633ca0613bdf17ff333063640354809da28a181
Sachiththa Bandaranayake <sacume3@gmail.com>
2022-08-12 14:45:32 +0200

    update typescript jest "@digitalbazzar" and install babel for jest typescript support.

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    test: {
+      presets: [
+        ["@babel/preset-env", { targets: { node: "current" } }],
+        "@babel/preset-typescript",
+      ],
+      plugins: ["@babel/plugin-transform-modules-commonjs"],
+    },
+  },
+};
