4b9cf78c57b0ac2a3d8fea81c445ed4b49638a61
Sachiththa Bandaranayake <sacume3@gmail.com>
2022-08-15 12:00:11 +0200

    fix jest not accepting node cli arguments

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
+const yargs = require("yargs");
+const argv = yargs(process.argv).parseSync();
+
+//Jest doesn't allow node cli arguments.
+//So node cli arguments is loaded and set to process.env before jest env is setup.
+process.env["TARGET_DID_TEST_DATA"] = argv.target || "default";
+
 module.exports = {
   testEnvironment: "node",
   transform: {
--- test/data/did-docs/did-docs.testdata.ts
+++ test/data/did-docs/did-docs.testdata.ts
@@ -1,5 +1,5 @@
-import fs = require("fs");
-import path = require("path");
+const fs = require("fs");
+const path = require("path");
 import yargs from "yargs";
 import { ALGORITHMS, KEY_FORMATS, KTYS } from "../../../src/core/globals";
 
@@ -57,38 +57,58 @@ interface DidTestData {
   };
 }
 
-const DEFAULT_TEST_DATA_FILE = "default.json";
+console.log(process.argv)
 
-function getDidTestData(filename: string): DidTestData {
-  const filepath = path.join(__dirname, filename);
-  const json = fs.readFileSync(filepath, { encoding: "utf-8" });
-  const testData = JSON.parse(json) as DidTestData;
-  validateDidTestData(testData);
-  return testData;
+class DidTestData {
+
+  private static DEFAULT_TEST_DATA_FILE = "default.json";
+  private static didTestDataList: DidTestData[] = [];
+
+  public static get testData(): DidTestData[] {
+    if (this.didTestDataList.length === 0) {
+      this.loadDidTestData();
+      console.log("loading")
+    }
+    console.log("Sdfsf")
+    return this.didTestDataList;
+  }
+
+  private static readDidTestData(filename: string): DidTestData {
+    const filepath = path.join(__dirname, filename);
+    const json = fs.readFileSync(filepath, { encoding: "utf-8" });
+    const testData = JSON.parse(json) as DidTestData;
+    this.validateDidTestData(testData);
+    return testData;
+  }
+
+  private static validateDidTestData(testData: any): void {
+    testData === null;
+  }
+
+  private static readDir(dirname: string): string[] {
+    return fs.readdirSync(dirname)
+      .filter((file: string) => path.extname(file).toLowerCase() === ".json");
+  }
+
+
+  private static loadDidTestData() {
+
+    //See jest.config.js
+    const arg = process.env?.TARGET_DID_TEST_DATA;
+
+    const files = this.readDir(__dirname)
+    if (arg) {
+      files
+        .map((filename: string) => this.readDidTestData(filename))
+        .filter((testData: DidTestData) => arg === testData.name || arg === "all")
+        .forEach((testData: DidTestData) => {
+          this.didTestDataList.push(testData);
+        });
+    } else {
+      this.didTestDataList.push(this.readDidTestData(this.DEFAULT_TEST_DATA_FILE));
+    }
+  }
+
 }
-
-function validateDidTestData(testData: any): void {
-  console.log(testData);
-}
-
-const argv = yargs(process.argv).argv;
-const files = fs
-  .readdirSync(__dirname)
-  .filter((file) => path.extname(file).toLowerCase() === ".json");
-const didTestDataList: DidTestData[] = [];
-const arg = argv["target"];
-if (arg) {
-  files
-    .map((filename) => {
-      return getDidTestData(filename);
-    })
-    .filter((testData) => arg === testData.name || arg === "all")
-    .forEach((testData) => {
-      didTestDataList.push(testData);
-    });
-} else {
-  const defaultTestData = getDidTestData(DEFAULT_TEST_DATA_FILE);
-  didTestDataList.push(defaultTestData);
-}
-
-export default didTestDataList;
+const a = DidTestData.testData
+export default a;
